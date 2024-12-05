a2fb3c6c02276d873cb91308c54f2250a37333cf
ccppoo <shmishmi79@gmail.com>
2024-12-05 23:35:12 +0900

    static images for dev

---
+++ src/static/index.ts
@@ -0,0 +1,20 @@
+import bluemap from './image/bluemap.webp';
+import discordIconBlue from './image/discord-mark-blue.svg';
+import discordIconWhite from './image/discord-mark-white.svg';
+import kakaoTalk from './image/kakaotalk_sharing_btn_medium_ov.png';
+import naverCafe from './image/naver_cafe.png';
+import chartSample from './samples/chart_example.png';
+
+const icon = {
+  discordIconWhite,
+  discordIconBlue,
+  naverCafe,
+  bluemap,
+  kakaoTalk,
+};
+
+const sample = {
+  chartSample,
+};
+
+export { icon, sample };
