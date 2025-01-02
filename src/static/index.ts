af748e4d5e00d3bc8d51a27236f0fe1cc1a3d948
ccppoo <shmishmi79@gmail.com>
2025-01-02 22:50:16 +0900

    add mc icon

--- src/static/index.ts
+++ src/static/index.ts
@@ -2,6 +2,7 @@ import bluemap from './image/bluemap.webp';
 import discordIconBlue from './image/discord-mark-blue.svg';
 import discordIconWhite from './image/discord-mark-white.svg';
 import kakaoTalk from './image/kakaotalk_sharing_btn_medium_ov.png';
+import minecraftIcon from './image/minecraft_logo_icon.svg';
 import naverCafe from './image/naver_cafe.png';
 import chartSample from './samples/chart_example.png';
 
@@ -11,6 +12,7 @@ const icon = {
   naverCafe,
   bluemap,
   kakaoTalk,
+  minecraftIcon,
 };
 
 const sample = {
