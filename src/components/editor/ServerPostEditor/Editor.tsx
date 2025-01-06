4529729199d78f0efff14de0daa4c506f4056416
ccppoo <shmishmi79@gmail.com>
2025-01-06 23:32:36 +0900

    a

--- src/components/editor/ServerPostEditor/Editor.tsx
+++ src/components/editor/ServerPostEditor/Editor.tsx
@@ -1,3 +1,6 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import type { DefaultValues, FieldPath, FieldValues, PathValue } from 'react-hook-form';
+
 import { Box, Button, Paper, Typography } from '@mui/material';
 
 import { useQuery } from '@tanstack/react-query';
@@ -6,9 +9,9 @@ import { useParams } from '@tanstack/react-router';
 import { FlexBox, FullSizeCenteredFlexBox } from '@/components/styled';
 import useUserProfile from '@/hooks/useUserProfile';
 
-import PostFormProvider from '../PostFormProvider';
 import EditorWrapper from '../components/Editor';
 import Title from '../components/Title';
+import api from './api';
 import type { ServerPostSchema } from './models';
 import { serverPostSchemaDefault } from './models';
 
@@ -16,7 +19,30 @@ import { serverPostSchemaDefault } from './models';
 
 // .ce-block .ce-block__content maxWidth가 보이는 화면 최대 넓이
 export default function ServerPostEditor({ data, readOnly }: { data?: any; readOnly?: boolean }) {
+  const methods = useForm<ServerPostSchema>({
+    defaultValues: data || serverPostSchemaDefault,
+  });
+
   const [userProfile] = useUserProfile();
+  const postID = methods.getValues('id' as FieldPath<ServerPostSchema>);
+  const isEditMode = !!postID;
+  // console.log(`methods.getValues('id') : ${methods.getValues('id')}`);
+
+  const submit = async (formData: ServerPostSchema) => {
+    const allValues = methods.getValues();
+    // const allValues = methods.
+    console.log(`data : ${JSON.stringify(allValues)}`);
+    if (isEditMode) {
+      await api.query.editBoardPost();
+      // await editBoardPost({ token: auth.id_token, data: allValues, postID: postID });
+    }
+    if (!isEditMode) {
+      await api.query.createBoardPost<ServerPostSchema>({ data: formData });
+      // await createBoardPost2<T>({ token: auth.id_token, data: allValues });
+      //   await AddNewTrack({ track: data });
+    }
+    return;
+  };
 
   const EDITOR_MODE = !!data
     ? !!readOnly
@@ -25,57 +51,60 @@ export default function ServerPostEditor({ data, readOnly }: { data?: any; readO
     : !readOnly
       ? 'WRITE MODE'
       : 'WTF';
+
   console.log(`EDITOR_MODE : ${EDITOR_MODE}`);
   const MAX_WIDTH = 750;
 
   return (
-    <PostFormProvider<ServerPostSchema> data={data || serverPostSchemaDefault}>
-      <FlexBox sx={{ flexDirection: 'column', alignItems: 'center', paddingY: 3, rowGap: 3 }}>
-        <FlexBox
-          sx={{
-            flexDirection: 'column',
-            rowGap: 2,
-            alignItems: 'center',
-            maxWidth: 1200,
-            width: '100%',
-          }}
-        >
-          {/* 제목 */}
-          <FlexBox sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-            <FlexBox sx={{ width: '100%', maxWidth: MAX_WIDTH }}>
-              <Title<ServerPostSchema> readOnly={!!readOnly} />
-            </FlexBox>
-          </FlexBox>
-          {/* 본문 */}
-          <Paper
-            sx={{
-              display: 'flex',
-              height: 'fit-content',
-              flexGrow: 1,
-              width: '100%',
-              maxWidth: MAX_WIDTH,
-            }}
-          >
-            <EditorWrapper<ServerPostSchema> readOnly={!!readOnly} />
-          </Paper>
-        </FlexBox>
-        {/* 글 작성, 취소 버튼 */}
-        {!readOnly && (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(submit)} style={{ width: '100%' }}>
+        <FlexBox sx={{ flexDirection: 'column', alignItems: 'center', paddingY: 3, rowGap: 3 }}>
           <FlexBox
             sx={{
+              flexDirection: 'column',
+              rowGap: 2,
+              alignItems: 'center',
+              maxWidth: 1200,
               width: '100%',
-              border: '1px black solid',
-              justifyContent: 'end',
-              padding: 2,
-              maxWidth: MAX_WIDTH,
             }}
           >
-            <Button variant="contained" type="submit">
-              submit
-            </Button>
+            {/* 제목 */}
+            <FlexBox sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <FlexBox sx={{ width: '100%', maxWidth: MAX_WIDTH }}>
+                <Title<ServerPostSchema> readOnly={!!readOnly} />
+              </FlexBox>
+            </FlexBox>
+            {/* 본문 */}
+            <Paper
+              sx={{
+                display: 'flex',
+                height: 'fit-content',
+                flexGrow: 1,
+                width: '100%',
+                maxWidth: MAX_WIDTH,
+              }}
+            >
+              <EditorWrapper<ServerPostSchema> readOnly={!!readOnly} />
+            </Paper>
           </FlexBox>
-        )}
-      </FlexBox>
-    </PostFormProvider>
+          {/* 글 작성, 취소 버튼 */}
+          {!readOnly && (
+            <FlexBox
+              sx={{
+                width: '100%',
+                border: '1px black solid',
+                justifyContent: 'end',
+                padding: 2,
+                maxWidth: MAX_WIDTH,
+              }}
+            >
+              <Button variant="contained" type="submit">
+                submit
+              </Button>
+            </FlexBox>
+          )}
+        </FlexBox>
+      </form>
+    </FormProvider>
   );
 }
--- src/components/editor/ServerPostEditor/models.ts
+++ src/components/editor/ServerPostEditor/models.ts
@@ -3,10 +3,33 @@ import { z } from 'zod';
 import { _outputDataSchema, boardPostMetadata, boardPostTitle } from '../models';
 import { postBase } from '../models/base';
 
+const minecraftVersion = z.array(z.string()).default([]);
+const minecraftLauncher = z.array(z.string()).default([]);
+
+const minecraftInfo = z.object({
+  version: minecraftVersion,
+  launcher: minecraftLauncher,
+});
+
+const serverInfo = z.object({
+  tags: z.array(z.string()).default([]),
+  serviceTime: z.string(),
+});
+
+const serverContact = z.object({
+  discord: z.optional(z.string()),
+  kakaoTalk: z.optional(z.string()),
+  naverCafe: z.optional(z.string()),
+  websites: z.array(z.string()).default([]),
+});
+
 const serverPostSchema = postBase
   .merge(_outputDataSchema)
   .merge(boardPostTitle)
-  .merge(boardPostMetadata);
+  .merge(boardPostMetadata)
+  .extend({ minecraftInfo: minecraftInfo })
+  .extend({ serverInfo: serverInfo })
+  .extend({ serverContact: serverContact });
 
 export type ServerPostSchema = z.input<typeof serverPostSchema>;
 
@@ -24,11 +47,22 @@ const DEFAULT_INITIAL_DATA = {
 
 export const serverPostSchemaDefault: ServerPostSchema = {
   id: undefined,
-  // comments: 0,
   data: DEFAULT_INITIAL_DATA,
   uploaded_at: undefined,
   modified_at: undefined,
-  // vote: undefined,
-  // user_id: undefined,
   title: '',
+  minecraftInfo: {
+    launcher: [],
+    version: [],
+  },
+  serverInfo: {
+    serviceTime: '',
+    tags: [],
+  },
+  serverContact: {
+    discord: undefined,
+    kakaoTalk: undefined,
+    naverCafe: undefined,
+    websites: [],
+  },
 };
