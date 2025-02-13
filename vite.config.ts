dee81d62a5cb2a1d60ae4e8754b7fb49fb205a3b
ccppoo <shmishmi79@gmail.com>
2025-02-13 14:24:30 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,54 @@
+/// <reference types="vite/client" />
 /// <reference types="vitest" />
 import * as path from 'path';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
-import { defineConfig } from 'vitest/config';
+import { defineConfig as testConfig } from 'vitest/config';
 
-export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), mkcert({})],
-  server: {
-    hmr: {
-      host: 'localhost',
-    },
-  },
-  envDir: './env',
+const certPath = path.resolve(__dirname, './certs/cert.pem');
+const keyPath = path.resolve(__dirname, './certs/key.pem');
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  const isDev = mode == 'dev';
+  const defaultPlugins = [TanStackRouterVite(), react()];
+  console.log(`isDev : ${isDev}`);
+
+  return {
+    // plugins: isDev ? [...defaultPlugins, mkcert({})] : defaultPlugins,
+    plugins: defaultPlugins,
+    server: {
+      hmr: {
+        host: isDev ? 'localhost' : undefined,
+      },
+      port: 5173,
+      https: {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      },
     },
-  },
-  test: {
-    root: path.resolve(__dirname, './src'),
-  },
+    preview: {
+      strictPort: true,
+      port: 5173,
+      https: {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      },
+    },
+    envDir: './env',
+    base: '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    test: {
+      root: path.resolve(__dirname, './src'),
+    },
+    build: {
+      minify: false,
+    },
+  };
 });
